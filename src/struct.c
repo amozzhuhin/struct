1473b06b47b8068363a2774332a914d6d0562eef
Mozzhuhin Andrey <nopscmn@gmail.com>
2013-07-21 21:37:16 +0400

    struct_unpack use struct_parse_field

--- src/struct.c
+++ src/struct.c
@@ -51,8 +51,7 @@ typedef struct _struct_context
 typedef ssize_t (*struct_pack_basic)(void *buffer, struct_context *context, va_list *vl);
 
 /** Definition of unpacker function for basic type */
-typedef ssize_t (*struct_unpack_basic)(const void *buffer, size_t size,
-		struct_context *context, va_list *vl);
+typedef ssize_t (*struct_unpack_basic)(const void *buffer, struct_context *context, va_list *vl);
 
 /** Definition of function for basic type size calculation */
 typedef ssize_t (*struct_calcsize_basic)(struct_context *context);
@@ -70,32 +69,40 @@ typedef struct _struct_format_field
 //
 
 static ssize_t struct_pack_pad(void *buffer, struct_context *context, va_list *vl);
+static ssize_t struct_unpack_pad(const void *buffer, struct_context *context, va_list *vl);
 static ssize_t struct_calcsize_pad(struct_context *context);
 
 static ssize_t struct_pack_byte(void *buffer, struct_context *context, va_list *vl);
+static ssize_t struct_unpack_byte(const void *buffer, struct_context *context, va_list *vl);
 static ssize_t struct_calcsize_byte(struct_context *context);
 
 static ssize_t struct_pack_bool(void *buffer, struct_context *context, va_list *vl);
+static ssize_t struct_unpack_bool(const void *buffer, struct_context *context, va_list *vl);
 static ssize_t struct_calcsize_bool(struct_context *context);
 
 static ssize_t struct_pack_short(void *buffer, struct_context *context, va_list *vl);
+static ssize_t struct_unpack_short(const void *buffer, struct_context *context, va_list *vl);
 static ssize_t struct_calcsize_short(struct_context *context);
 
 static ssize_t struct_pack_int(void *buffer, struct_context *context, va_list *vl);
+static ssize_t struct_unpack_int(const void *buffer, struct_context *context, va_list *vl);
 static ssize_t struct_calcsize_int(struct_context *context);
 
 static ssize_t struct_pack_quad(void *buffer, struct_context *context, va_list *vl);
+static ssize_t struct_unpack_quad(const void *buffer, struct_context *context, va_list *vl);
 static ssize_t struct_calcsize_quad(struct_context *context);
 
 static ssize_t struct_pack_float(void *buffer, struct_context *context, va_list *vl);
+static ssize_t struct_unpack_float(const void *buffer, struct_context *context, va_list *vl);
 static ssize_t struct_calcsize_float(struct_context *context);
 
 static ssize_t struct_pack_double(void *buffer, struct_context *context, va_list *vl);
+static ssize_t struct_unpack_double(const void *buffer, struct_context *context, va_list *vl);
 static ssize_t struct_calcsize_double(struct_context *context);
 
 static ssize_t struct_pack_str(void *buffer, struct_context *context, va_list *vl);
+static ssize_t struct_unpack_str(const void *buffer, struct_context *context, va_list *vl);
 static ssize_t struct_calcsize_str(struct_context *context);
-
 
 //
 // Private Variables
@@ -103,22 +110,22 @@ static ssize_t struct_calcsize_str(struct_context *context);
 
 /** Format definition table */
 static const struct_format_field struct_format_fields[] = {
-		{ 'x', struct_pack_pad, NULL, struct_calcsize_pad },
-		{ 'c', struct_pack_byte, NULL, struct_calcsize_byte },
-		{ 'b', struct_pack_byte, NULL, struct_calcsize_byte },
-		{ 'B', struct_pack_byte, NULL, struct_calcsize_byte },
-		{ '?', struct_pack_bool, NULL, struct_calcsize_bool },
-		{ 'h', struct_pack_short, NULL, struct_calcsize_short },
-		{ 'H', struct_pack_short, NULL, struct_calcsize_short },
-		{ 'i', struct_pack_int, NULL, struct_calcsize_int },
-		{ 'I', struct_pack_int, NULL, struct_calcsize_int },
-		{ 'l', struct_pack_int, NULL, struct_calcsize_int },
-		{ 'L', struct_pack_int, NULL, struct_calcsize_int },
-		{ 'q', struct_pack_quad, NULL, struct_calcsize_quad },
-		{ 'Q', struct_pack_quad, NULL, struct_calcsize_quad },
-		{ 'f', struct_pack_float, NULL, struct_calcsize_float },
-		{ 'd', struct_pack_double, NULL, struct_calcsize_double },
-		{ 's', struct_pack_str, NULL, struct_calcsize_str },
+		{ 'x', struct_pack_pad, struct_unpack_pad, struct_calcsize_pad },
+		{ 'c', struct_pack_byte, struct_unpack_byte, struct_calcsize_byte },
+		{ 'b', struct_pack_byte, struct_unpack_byte, struct_calcsize_byte },
+		{ 'B', struct_pack_byte, struct_unpack_byte, struct_calcsize_byte },
+		{ '?', struct_pack_bool, struct_unpack_bool, struct_calcsize_bool },
+		{ 'h', struct_pack_short, struct_unpack_short, struct_calcsize_short },
+		{ 'H', struct_pack_short, struct_unpack_short, struct_calcsize_short },
+		{ 'i', struct_pack_int, struct_unpack_int, struct_calcsize_int },
+		{ 'I', struct_pack_int, struct_unpack_int, struct_calcsize_int },
+		{ 'l', struct_pack_int, struct_unpack_int, struct_calcsize_int },
+		{ 'L', struct_pack_int, struct_unpack_int, struct_calcsize_int },
+		{ 'q', struct_pack_quad, struct_unpack_quad, struct_calcsize_quad },
+		{ 'Q', struct_pack_quad, struct_unpack_quad, struct_calcsize_quad },
+		{ 'f', struct_pack_float, struct_unpack_float, struct_calcsize_float },
+		{ 'd', struct_pack_double, struct_unpack_double, struct_calcsize_double },
+		{ 's', struct_pack_str, struct_unpack_str, struct_calcsize_str },
 		/* end of format table */
 		{ '\0', NULL, NULL, NULL }
 };
@@ -130,6 +137,11 @@ static const struct_format_field struct_format_fields[] = {
 static ssize_t struct_pack_pad(void *buffer, struct_context *context, va_list *vl)
 {
 	memset(buffer, 0, context->repeat);
+	return context->repeat * sizeof(uint8_t);
+}
+
+static ssize_t struct_unpack_pad(const void *buffer, struct_context *context, va_list *vl)
+{
 	return context->repeat * sizeof(uint8_t);
 }
 
@@ -149,20 +161,42 @@ static ssize_t struct_pack_byte(void *buffer, struct_context *context, va_list *
 	return context->repeat * sizeof(uint8_t);
 }
 
+static ssize_t struct_unpack_byte(const void *buffer, struct_context *context, va_list *vl)
+{
+	const int8_t *p = buffer;
+	int i;
+
+	for (i = 0; i < context->repeat; i++)
+		*va_arg(*vl, int8_t*) = *p++;
+
+	return context->repeat * sizeof(int8_t);
+}
+
 static ssize_t struct_calcsize_byte(struct_context *context)
 {
-	return context->repeat * sizeof(uint8_t);
+	return context->repeat * sizeof(int8_t);
 }
 
 static ssize_t struct_pack_bool(void *buffer, struct_context *context, va_list *vl)
 {
-	uint8_t *p = buffer;
+	int8_t *p = buffer;
 	int i;
 
 	for (i = 0; i < context->repeat; i++)
 		*p++ = va_arg(*vl, int) != 0;
 
-	return context->repeat * sizeof(uint8_t);
+	return context->repeat * sizeof(int8_t);
+}
+
+static ssize_t struct_unpack_bool(const void *buffer, struct_context *context, va_list *vl)
+{
+	const int8_t *p = buffer;
+	int i;
+
+	for (i = 0; i < context->repeat; i++)
+		*va_arg(*vl, int8_t*) = (*p++ != 0);
+
+	return context->repeat * sizeof(int8_t);
 }
 
 static ssize_t struct_calcsize_bool(struct_context *context)
@@ -172,11 +206,22 @@ static ssize_t struct_calcsize_bool(struct_context *context)
 
 static ssize_t struct_pack_short(void *buffer, struct_context *context, va_list *vl)
 {
-	uint16_t *p = buffer;
+	int16_t *p = buffer;
 	int i;
 
 	for (i = 0; i < context->repeat; i++)
 		*p++ = va_arg(*vl, int);
+
+	return context->repeat * sizeof(int16_t);
+}
+
+static ssize_t struct_unpack_short(const void *buffer, struct_context *context, va_list *vl)
+{
+	const int16_t *p = buffer;
+	int i;
+
+	for (i = 0; i < context->repeat; i++)
+		*va_arg(*vl, int16_t*) = *p++;
 
 	return context->repeat * sizeof(int16_t);
 }
@@ -197,6 +242,17 @@ static ssize_t struct_pack_int(void *buffer, struct_context *context, va_list *v
 	return context->repeat * sizeof(int32_t);
 }
 
+static ssize_t struct_unpack_int(const void *buffer, struct_context *context, va_list *vl)
+{
+	const int32_t *p = buffer;
+	int i;
+
+	for (i = 0; i < context->repeat; i++)
+		*va_arg(*vl, int32_t*) = *p++;
+
+	return context->repeat * sizeof(int32_t);
+}
+
 static ssize_t struct_calcsize_int(struct_context *context)
 {
 	return context->repeat * sizeof(int32_t);
@@ -213,6 +269,17 @@ static ssize_t struct_pack_quad(void *buffer, struct_context *context, va_list *
 	return context->repeat * sizeof(int64_t);
 }
 
+static ssize_t struct_unpack_quad(const void *buffer, struct_context *context, va_list *vl)
+{
+	const int64_t *p = buffer;
+	int i;
+
+	for (i = 0; i < context->repeat; i++)
+		*va_arg(*vl, int64_t*) = *p++;
+
+	return context->repeat * sizeof(int64_t);
+}
+
 static ssize_t struct_calcsize_quad(struct_context *context)
 {
 	return context->repeat * sizeof(int64_t);
@@ -220,12 +287,22 @@ static ssize_t struct_calcsize_quad(struct_context *context)
 
 static ssize_t struct_pack_float(void *buffer, struct_context *context, va_list *vl)
 {
-	ssize_t result = -1;
 	float *p = buffer;
 	int i;
 
 	for (i = 0; i < context->repeat; i++)
 		*p++ = va_arg(*vl, double);
+
+	return context->repeat * sizeof(float);
+}
+
+static ssize_t struct_unpack_float(const void *buffer, struct_context *context, va_list *vl)
+{
+	const float *p = buffer;
+	int i;
+
+	for (i = 0; i < context->repeat; i++)
+		*va_arg(*vl, float*) = *p++;
 
 	return context->repeat * sizeof(float);
 }
@@ -242,6 +319,17 @@ static ssize_t struct_pack_double(void *buffer, struct_context *context, va_list
 
 	for (i = 0; i < context->repeat; i++)
 		*p++ = va_arg(*vl, double);
+
+	return context->repeat * sizeof(double);
+}
+
+static ssize_t struct_unpack_double(const void *buffer, struct_context *context, va_list *vl)
+{
+	const double *p = buffer;
+	int i;
+
+	for (i = 0; i < context->repeat; i++)
+		*va_arg(*vl, double*) = *p++;
 
 	return context->repeat * sizeof(double);
 }
@@ -264,6 +352,17 @@ static ssize_t struct_pack_str(void *buffer, struct_context *context, va_list *v
 		else
 			*p++ = 0;
 	}
+
+	return context->repeat * sizeof(char);
+}
+
+static ssize_t struct_unpack_str(const void *buffer, struct_context *context, va_list *vl)
+{
+	char *str = va_arg(*vl, char *);
+	size_t str_size = va_arg(*vl, size_t);
+
+	strncpy(str, buffer, context->repeat < str_size ? context->repeat : str_size);
+	str[str_size - 1] = '\0';
 
 	return context->repeat * sizeof(char);
 }
@@ -335,7 +434,7 @@ ssize_t struct_pack(void *buffer, size_t size, const char *format, ...)
 	p = buffer;
 	va_start(vl, format);
 
-	while ((*c != 0) && (p < (uint8_t *) buffer + size))
+	while ((*c != '\0') && (p < (uint8_t *) buffer + size))
 	{
 		c = struct_parse_field(c, &context, &field);
 		if (field != NULL && (uint8_t *) buffer + size - p >= field->calcsize(&context))
@@ -347,7 +446,7 @@ ssize_t struct_pack(void *buffer, size_t size, const char *format, ...)
 	va_end(vl);
 
 	// format string not processed
-	if (*c != 0)
+	if (*c != '\0')
 		return -1;
 
 	return p - (uint8_t *) buffer;
@@ -356,126 +455,35 @@ ssize_t struct_pack(void *buffer, size_t size, const char *format, ...)
 ssize_t struct_unpack(const void *buffer, size_t size, const char *format, ...)
 {
 	const char *c;
+	struct_context context;
+	const struct_format_field *field;
 	const uint8_t *p;
 	va_list vl;
-	int repeat;
 
 	if (buffer == NULL || format == NULL)
 		return -1;
+
+	context.byte_order = __BYTE_ORDER;
+	context.native_alignment = 1;
+	context.native_size = 1;
 
 	c = format;
 	p = buffer;
 	va_start(vl, format);
 
-	while ((*c != 0) && (p - (uint8_t *) buffer < size))
+	while ((*c != '\0') && (p < (uint8_t *) buffer + size))
 	{
-		// whitespace characters between formats are ignored
-		if (*c == ' ' || *c == '\t')
-		{
-			c++;
-			continue;
-		}
-		// read integral repeat count
-		if (*c >= '0' && *c <= '9')
-		{
-			repeat = 0;
-			do {
-				repeat = repeat * 10 + *c - '0';
-				c++;
-			} while (*c >= '0' && *c <= '9');
-		}
+		c = struct_parse_field(c, &context, &field);
+		if (field != NULL && (const uint8_t *) buffer + size - p >= field->calcsize(&context))
+			p += field->unpack(p, &context, &vl);
 		else
-		{
-			repeat = 1;
-		}
-		// process format character
-		switch (*c)
-		{
-		case 'x':
-			while (repeat-- > 0) p++;
 			break;
-		case 'c':
-		case 'b':
-		case 'B':
-			while (repeat-- > 0) *va_arg(vl, uint8_t*) = *p++;
-			break;
-		case '?':
-			while (repeat-- > 0) *va_arg(vl, uint8_t*) = *p++ != 0;
-			break;
-		case 'h':
-		case 'H':
-			while (repeat-- > 0)
-			{
-				*va_arg(vl, int*) = *(int16_t*)p;
-				p += sizeof(int16_t);
-			}
-			break;
-		case 'i':
-		case 'I':
-		case 'l':
-		case 'L':
-			while (repeat-- > 0)
-			{
-				*va_arg(vl, int32_t*) = *(int32_t*) p;
-				p += sizeof(int32_t);
-			}
-			break;
-		case 'q':
-		case 'Q':
-			while (repeat-- > 0)
-			{
-				*va_arg(vl, uint64_t*) = *(int64_t*)p;
-				p += sizeof(int64_t);
-			}
-			break;
-		case 'f':
-			while (repeat-- > 0)
-			{
-				*va_arg(vl, float*) = *(float*)p;
-				p += sizeof(float);
-			}
-			break;
-		case 'd':
-			while (repeat-- > 0)
-			{
-				*va_arg(vl, double*) = *(double*)p;
-				p += sizeof(double);
-			}
-			break;
-		case 's':
-			{
-				char *str = va_arg(vl, char *);
-				size_t str_size = va_arg(vl, size_t);
-
-				while (repeat-- > 0)
-				{
-					if (str_size > 0)
-					{
-						*str++ = *p++;
-						str_size--;
-					}
-					else
-					{
-						p++;
-					}
-				}
-
-				if (str_size <= 0)
-					str--;
-				*str = '\0';
-			}
-			break;
-		default:
-			// invalid character
-			break;
-		}
-		c++;
 	}
 
 	va_end(vl);
 
 	// not processed all format string
-	if (*c != 0)
+	if (*c != '\0')
 		return -1;
 
 	return p - (uint8_t *) buffer;
@@ -498,7 +506,7 @@ ssize_t struct_calcsize(const char *format)
 	c = format;
 	result = 0;
 
-	while (*c != 0)
+	while (*c != '\0')
 	{
 		c = struct_parse_field(c, &context, &field);
 		if (field != NULL)
@@ -508,7 +516,7 @@ ssize_t struct_calcsize(const char *format)
 	}
 
 	// format string not processed
-	if (*c != 0)
+	if (*c != '\0')
 		return -1;
 
 	return result;
